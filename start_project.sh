687033d3d87934749490992b2e965c7e61cd7b97
Daniel Nocito Merodio <115790319+dnocitomerodio@users.noreply.github.com>
2025-01-11 00:34:20 +0100

    Update start_project.sh

--- start_project.sh
+++ start_project.sh
@@ -1,7 +1,11 @@
-pip install -r requirements.txt 
+#!/bin/bash
+echo "Downloading dependencies..."
+pip install -r requirements.txt
 
+echo "Starting API Flask..."
 cd api
-FLASK_APP=app.py FLASK_ENV=developement flask run &
+FLASK_APP=app.py FLASK_ENV=development flask run &
 
 cd ..
+echo "Initializing voice assistant script."
 python3 script/voice.py 2>/dev/null
